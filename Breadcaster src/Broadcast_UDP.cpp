156fce85b66629adb6d72e4e1e9b5a6cc130eebb
Rick-laboratory <59580966+Rick-laboratory@users.noreply.github.com>
2021-11-03 09:42:20 +0100

    Rollback

--- Breadcaster src/Broadcast_UDP.cpp
+++ Breadcaster src/Broadcast_UDP.cpp
@@ -31,5 +31,8 @@ Broadcast_UDP::~Broadcast_UDP()
 
 void Broadcast_UDP::Broadcast(int message[])
 {
-	sendto(m_socket, (char*)&message, 3 * sizeof(int), 0, (sockaddr*)&m_send_addr,sizeof(m_send_addr));
+	var[0] = message[0];
+	var[1] = message[1];
+	var[2] = message[2];
+	sendto(m_socket, (char*)&var, sizeof(var), 0, (sockaddr*)&m_send_addr,sizeof(m_send_addr));
 }
